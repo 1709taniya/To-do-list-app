6483d6261043efe43fbd414ae17c8a141ef9e660
Taniya panchal <panchal6566@gamil.com>
2026-06-29 13:23:31 +0530

    Implement task completion tracking

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
-let enter=document.querySelector("#entertask");
-let button=document.querySelector(".btn");
+let enter = document.querySelector("#entertask");
+let button = document.querySelector(".btn");
+let list = document.getElementById("list");
 
-button.addEventListener("click",function(){
-    if(enter.value.trim() !== ""){
-    const tasklist=document.createElement("li");
-    document.getElementById("list").append(tasklist);
-    tasklist.innerHTML=enter.value;
-    enter.value="";
-    deleteele(tasklist)
-}});
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-function deleteele(tasklist){
-    let dlt=document.createElement("button");
-    dlt.textContent="Delete";
-    tasklist.append(dlt);
-    dlt.addEventListener("click",function(){
-    tasklist.remove();
+tasks.forEach((task) => {
+  const tasklist = document.createElement("li");
+  list.append(tasklist);
+  createTask(tasklist, task);
 });
-};
 
-enter.addEventListener("keydown",function(e){
-    if(e.key==="Enter"){
-        button.click();
+button.addEventListener("click", function () {
+  if (enter.value.trim() !== "") {
+    let task = {
+      text: enter.value,
+      completed: false,
     };
+
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    const tasklist = document.createElement("li");
+    list.append(tasklist);
+    createTask(tasklist, task);
+
+    enter.value = "";
+  }
+});
+
+function createTask(tasklist, task) {
+  let check = document.createElement("input");
+  check.type = "checkbox";
+  check.id='checkbox'
+  check.checked = task.completed;
+
+  let text = document.createElement("span");
+  text.textContent = task.text;
+
+  let dlt = document.createElement("button");
+  dlt.id='dlt-task'
+  dlt.textContent = "Delete";
+
+  let left = document.createElement("div");
+  left.classList.add("left");
+
+  left.append(check);
+  left.append(text);
+
+  tasklist.append(left);
+  tasklist.append(dlt);
+
+  if (task.completed) {
+    left.style.textDecoration = "line-through";
+    left.style.opacity = "0.6";
+  }
+
+  check.addEventListener("change", () => {
+    task.completed = check.checked;
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    if (check.checked) {
+      left.style.textDecoration = "line-through";
+      left.style.opacity = "0.6";
+    } else {
+      tasklist.style.textDecoration = "none";
+      tasklist.style.opacity = "1";
+    }
+  });
+
+  dlt.addEventListener("click", () => {
+    tasks = tasks.filter((t) => t !== task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    tasklist.remove();
+  });
+}
+
+enter.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    button.click();
+  }
 });
